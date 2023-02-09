f9c9269dcab5814423718e2f2ad1fb9339c2cfde
Shaina Couch <shainacouch21@gmail.com>
2023-02-09 16:00:21 -0600

    updated fixtures

--- conditions_report_api/conditionsapi/models/OutsideConditions.py
+++ conditions_report_api/conditionsapi/models/OutsideConditions.py
@@ -2,3 +2,4 @@ from django.db import models
 
 class OutsideConditions(models.Model):
     condition = models.CharField(max_length=150)
+    isGood = models.BooleanField(default=False)
--- conditions_report_api/conditionsapi/models/RockConditions.py
+++ conditions_report_api/conditionsapi/models/RockConditions.py
@@ -2,3 +2,4 @@ from django.db import models
 
 class RockConditions(models.Model):
     condition = models.CharField(max_length=150)
+    isGood = models.BooleanField(default=False)
